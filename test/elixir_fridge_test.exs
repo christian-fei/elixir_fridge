b800281b3c1702feebf7aea59a89861a21d1c8c8
Christian Fei <crifei93@gmail.com>
2015-10-23 00:31:02 +0200

    refactoring with setup hook

--- test/elixir_fridge_test.exs
+++ test/elixir_fridge_test.exs
@@ -2,19 +2,21 @@ defmodule FridgeTest do
   use ExUnit.Case
   doctest Fridge
 
-  test "fridge is empty" do
+  setup do
     {:ok, fridge} = Fridge.start_link()
+    {:ok, fridge: fridge}
+  end
+
+  test "fridge is empty", %{fridge: fridge} do
     assert [] == Fridge.get_items(fridge)
   end
 
-  test "adding an item to the fridge updates its contents" do
-    {:ok, fridge} = Fridge.start_link()
+  test "adding an item to the fridge updates its contents", %{fridge: fridge} do
     Fridge.add_item(fridge, "yogurt")
     assert ["yogurt"] == Fridge.get_items(fridge)
   end
 
-  test "removing an items from the fridge updates its contents" do
-    {:ok, fridge} = Fridge.start_link()
+  test "removing an items from the fridge updates its contents", %{fridge: fridge} do
     Fridge.add_item(fridge, "yogurt")
     Fridge.remove_item(fridge, "yogurt")
     assert [] == Fridge.get_items(fridge)
